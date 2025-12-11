0e9799ef6b89d3aaa1d3b40842994038c9920928
ahperagon <ahperagon@gmail.com>
2025-12-11 09:51:33 +0100

    Add files via upload

---
+++ Prueba/chiste/index.php
@@ -0,0 +1,3 @@
+<?php
+echo "<h1>Un byte le dice a otro: ¿Quieres salir esta noche? — Lo siento, estoy ocupado.</h1>";
+?>
---
+++ Prueba/encuesta/index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1>¿Independizar Linares de la provincia de Jaén?</h1>";
+echo "<div>
+        <button>Sí</button>
+        <button>No</button>
+      </div>";
+?>
